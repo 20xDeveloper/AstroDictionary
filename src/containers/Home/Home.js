8007ae0ae5314bf0a10254038c5563ecec3b45a0
Lami <AsaadulLamiKabir@gmail.com>
2022-12-07 17:00:45 -0500

    saving changes.

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "./Home.css"
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <Button variant="primary">Primary</Button>
+            <div class="container">
+                <Form.Control className="main-search-bar" type="text" placeholder="Normal text" />
             </div>
         )
     }
